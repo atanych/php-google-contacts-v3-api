458842d8eb39c054fe5b146f423b5d53f2bd3251
atanych <atanych@gmail.com>
2016-04-30 20:00:39 +0300

    fix auth app

--- authorise-application.php
+++ authorise-application.php
@@ -5,11 +5,17 @@
 
 // Note that the redirectUri value should point towards a hosted version of 'redirect_handler.php'.
 
-require_once '../../../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 use rapidweb\googlecontacts\helpers\GoogleHelper;
 
-$client = GoogleHelper::getClient();
+$client = GoogleHelper::getClient([
+    "clientID"     => "739387131167-7vr4pib0qknl2dda1su1gimn25i9cgl6.apps.googleusercontent.com",
+    "clientSecret" => "GL4gDBhMrHXczl6JNJG5R2Tt",
+    "redirectUri"  => "https://chat2desk.com/site/google",
+    "developerKey" => "ya29.CjHUAirRY712x6riA3xVBp2MD2OzLvEi5bgboT5yzA-e4CSVe7bHcQZb4hy6OKzGjHD2",
+    "refreshToken" => "1/kcSzMQflgfbjO4YaIoeRM1VJy31pk4gyON0v6t9v4GQ"
+]);
 
 $authUrl = GoogleHelper::getAuthUrl($client);
 
